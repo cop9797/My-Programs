79d3aff7e511860c21bc94252f17d612bbb68d5f
cop9797 <hhcop9797@gmail.com>
2016-09-20 12:14:19 -0600

    More Programs added

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,392 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ModifyButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ButtonSeven = New System.Windows.Forms.Button()
+        Me.ButtonEight = New System.Windows.Forms.Button()
+        Me.ButtonNine = New System.Windows.Forms.Button()
+        Me.ButtonFour = New System.Windows.Forms.Button()
+        Me.ButtonFive = New System.Windows.Forms.Button()
+        Me.ButtonSix = New System.Windows.Forms.Button()
+        Me.ButtonOne = New System.Windows.Forms.Button()
+        Me.ButtonTwo = New System.Windows.Forms.Button()
+        Me.ButtonThree = New System.Windows.Forms.Button()
+        Me.ButtonZero = New System.Windows.Forms.Button()
+        Me.ButtonPlus = New System.Windows.Forms.Button()
+        Me.ButtonMultiply = New System.Windows.Forms.Button()
+        Me.ButtonMinus = New System.Windows.Forms.Button()
+        Me.ButtonExponent = New System.Windows.Forms.Button()
+        Me.ButtonDevide = New System.Windows.Forms.Button()
+        Me.ButtonEqual = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ModifyButton
+        '
+        Me.ModifyButton.Location = New System.Drawing.Point(607, 192)
+        Me.ModifyButton.Name = "ModifyButton"
+        Me.ModifyButton.Size = New System.Drawing.Size(75, 23)
+        Me.ModifyButton.TabIndex = 0
+        Me.ModifyButton.Text = "Modify"
+        Me.ModifyButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(3, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(782, 172)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ButtonSeven
+        '
+        Me.ButtonSeven.Location = New System.Drawing.Point(13, 192)
+        Me.ButtonSeven.Name = "ButtonSeven"
+        Me.ButtonSeven.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonSeven.TabIndex = 2
+        Me.ButtonSeven.Text = "7"
+        Me.ButtonSeven.UseVisualStyleBackColor = True
+        '
+        'ButtonEight
+        '
+        Me.ButtonEight.Location = New System.Drawing.Point(57, 192)
+        Me.ButtonEight.Name = "ButtonEight"
+        Me.ButtonEight.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonEight.TabIndex = 3
+        Me.ButtonEight.Text = "8"
+        Me.ButtonEight.UseVisualStyleBackColor = True
+        '
+        'ButtonNine
+        '
+        Me.ButtonNine.Location = New System.Drawing.Point(101, 191)
+        Me.ButtonNine.Name = "ButtonNine"
+        Me.ButtonNine.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonNine.TabIndex = 4
+        Me.ButtonNine.Text = "9"
+        Me.ButtonNine.UseVisualStyleBackColor = True
+        '
+        'ButtonFour
+        '
+        Me.ButtonFour.Location = New System.Drawing.Point(13, 221)
+        Me.ButtonFour.Name = "ButtonFour"
+        Me.ButtonFour.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonFour.TabIndex = 5
+        Me.ButtonFour.Text = "4"
+        Me.ButtonFour.UseVisualStyleBackColor = True
+        '
+        'ButtonFive
+        '
+        Me.ButtonFive.Location = New System.Drawing.Point(57, 221)
+        Me.ButtonFive.Name = "ButtonFive"
+        Me.ButtonFive.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonFive.TabIndex = 6
+        Me.ButtonFive.Text = "5"
+        Me.ButtonFive.UseVisualStyleBackColor = True
+        '
+        'ButtonSix
+        '
+        Me.ButtonSix.Location = New System.Drawing.Point(101, 221)
+        Me.ButtonSix.Name = "ButtonSix"
+        Me.ButtonSix.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonSix.TabIndex = 7
+        Me.ButtonSix.Text = "6"
+        Me.ButtonSix.UseVisualStyleBackColor = True
+        '
+        'ButtonOne
+        '
+        Me.ButtonOne.Location = New System.Drawing.Point(13, 250)
+        Me.ButtonOne.Name = "ButtonOne"
+        Me.ButtonOne.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonOne.TabIndex = 8
+        Me.ButtonOne.Text = "1"
+        Me.ButtonOne.UseVisualStyleBackColor = True
+        '
+        'ButtonTwo
+        '
+        Me.ButtonTwo.Location = New System.Drawing.Point(57, 250)
+        Me.ButtonTwo.Name = "ButtonTwo"
+        Me.ButtonTwo.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonTwo.TabIndex = 9
+        Me.ButtonTwo.Text = "2"
+        Me.ButtonTwo.UseVisualStyleBackColor = True
+        '
+        'ButtonThree
+        '
+        Me.ButtonThree.Location = New System.Drawing.Point(101, 250)
+        Me.ButtonThree.Name = "ButtonThree"
+        Me.ButtonThree.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonThree.TabIndex = 10
+        Me.ButtonThree.Text = "3"
+        Me.ButtonThree.UseVisualStyleBackColor = True
+        '
+        'ButtonZero
+        '
+        Me.ButtonZero.Location = New System.Drawing.Point(13, 279)
+        Me.ButtonZero.Name = "ButtonZero"
+        Me.ButtonZero.Size = New System.Drawing.Size(126, 23)
+        Me.ButtonZero.TabIndex = 11
+        Me.ButtonZero.Text = "0"
+        Me.ButtonZero.UseVisualStyleBackColor = True
+        '
+        'ButtonPlus
+        '
+        Me.ButtonPlus.Location = New System.Drawing.Point(200, 191)
+        Me.ButtonPlus.Name = "ButtonPlus"
+        Me.ButtonPlus.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonPlus.TabIndex = 12
+        Me.ButtonPlus.Text = "+"
+        Me.ButtonPlus.UseVisualStyleBackColor = True
+        '
+        'ButtonMultiply
+        '
+        Me.ButtonMultiply.Location = New System.Drawing.Point(244, 191)
+        Me.ButtonMultiply.Name = "ButtonMultiply"
+        Me.ButtonMultiply.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonMultiply.TabIndex = 13
+        Me.ButtonMultiply.Text = "*"
+        Me.ButtonMultiply.UseVisualStyleBackColor = True
+        '
+        'ButtonMinus
+        '
+        Me.ButtonMinus.Location = New System.Drawing.Point(200, 220)
+        Me.ButtonMinus.Name = "ButtonMinus"
+        Me.ButtonMinus.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonMinus.TabIndex = 14
+        Me.ButtonMinus.Text = "-"
+        Me.ButtonMinus.UseVisualStyleBackColor = True
+        '
+        'ButtonExponent
+        '
+        Me.ButtonExponent.Location = New System.Drawing.Point(244, 220)
+        Me.ButtonExponent.Name = "ButtonExponent"
+        Me.ButtonExponent.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonExponent.TabIndex = 15
+        Me.ButtonExponent.Text = "^"
+        Me.ButtonExponent.UseVisualStyleBackColor = True
+        '
+        'ButtonDevide
+        '
+        Me.ButtonDevide.Location = New System.Drawing.Point(200, 249)
+        Me.ButtonDevide.Name = "ButtonDevide"
+        Me.ButtonDevide.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonDevide.TabIndex = 16
+        Me.ButtonDevide.Text = "/"
+        Me.ButtonDevide.UseVisualStyleBackColor = True
+        '
+        'ButtonEqual
+        '
+        Me.ButtonEqual.Location = New System.Drawing.Point(244, 249)
+        Me.ButtonEqual.Name = "ButtonEqual"
+        Me.ButtonEqual.Size = New System.Drawing.Size(38, 23)
+        Me.ButtonEqual.TabIndex = 17
+        Me.ButtonEqual.Text = "="
+        Me.ButtonEqual.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(13, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(785, 173)
+        Me.Panel1.TabIndex = 19
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(711, 188)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(810, 351)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ButtonEqual)
+        Me.Controls.Add(Me.ButtonDevide)
+        Me.Controls.Add(Me.ButtonExponent)
+        Me.Controls.Add(Me.ButtonMinus)
+        Me.Controls.Add(Me.ButtonMultiply)
+        Me.Controls.Add(Me.ButtonPlus)
+        Me.Controls.Add(Me.ButtonZero)
+        Me.Controls.Add(Me.ButtonThree)
+        Me.Controls.Add(Me.ButtonTwo)
+        Me.Controls.Add(Me.ButtonOne)
+        Me.Controls.Add(Me.ButtonSix)
+        Me.Controls.Add(Me.ButtonFive)
+        Me.Controls.Add(Me.ButtonFour)
+        Me.Controls.Add(Me.ButtonNine)
+        Me.Controls.Add(Me.ButtonEight)
+        Me.Controls.Add(Me.ButtonSeven)
+        Me.Controls.Add(Me.ModifyButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ModifyButton As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonSeven As System.Windows.Forms.Button
+    Friend WithEvents ButtonEight As System.Windows.Forms.Button
+    Friend WithEvents ButtonNine As System.Windows.Forms.Button
+    Friend WithEvents ButtonFour As System.Windows.Forms.Button
+    Friend WithEvents ButtonFive As System.Windows.Forms.Button
+    Friend WithEvents ButtonSix As System.Windows.Forms.Button
+    Friend WithEvents ButtonOne As System.Windows.Forms.Button
+    Friend WithEvents ButtonTwo As System.Windows.Forms.Button
+    Friend WithEvents ButtonThree As System.Windows.Forms.Button
+    Friend WithEvents ButtonZero As System.Windows.Forms.Button
+    Friend WithEvents ButtonPlus As System.Windows.Forms.Button
+    Friend WithEvents ButtonMultiply As System.Windows.Forms.Button
+    Friend WithEvents ButtonMinus As System.Windows.Forms.Button
+    Friend WithEvents ButtonExponent As System.Windows.Forms.Button
+    Friend WithEvents ButtonDevide As System.Windows.Forms.Button
+    Friend WithEvents ButtonEqual As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim currentLineIndex As Integer = TextBox1.GetLineFromCharIndex(TextBox1.SelectionStart)
+        Dim lastLineIndex As Integer = TextBox1.GetLineFromCharIndex(TextBox1.TextLength)
+        Dim currentColumnIndex As Integer = TextBox1.SelectionStart - TextBox1.GetFirstCharIndexFromLine(currentLineIndex)
+        Label1.Text = String.Format("Line {0}/{1}, Column {2}", currentLineIndex + 1, lastLineIndex + 1, currentColumnIndex + 1)
+    End Sub
+
+    Private Sub ButtonOne_Click(sender As Object, e As EventArgs) Handles ButtonOne.Click
+        TextBox1.Text += "1"
+    End Sub
+
+    Private Sub ButtonTwo_Click(sender As Object, e As EventArgs) Handles ButtonTwo.Click
+        TextBox1.Text += "2"
+    End Sub
+
+    Private Sub ButtonThree_Click(sender As Object, e As EventArgs) Handles ButtonThree.Click
+        TextBox1.Text += "3"
+    End Sub
+
+    Private Sub ButtonFour_Click(sender As Object, e As EventArgs) Handles ButtonFour.Click
+        TextBox1.Text += "4"
+    End Sub
+
+    Private Sub ButtonFive_Click(sender As Object, e As EventArgs) Handles ButtonFive.Click
+        TextBox1.Text += "5"
+    End Sub
+
+    Private Sub ButtonSix_Click(sender As Object, e As EventArgs) Handles ButtonSix.Click
+        TextBox1.Text += "6"
+    End Sub
+
+    Private Sub ButtonSeven_Click(sender As Object, e As EventArgs) Handles ButtonSeven.Click
+        TextBox1.Text += "7"
+    End Sub
+
+    Private Sub ButtonEight_Click(sender As Object, e As EventArgs) Handles ButtonEight.Click
+        TextBox1.Text += "8"
+    End Sub
+
+    Private Sub ButtonNine_Click(sender As Object, e As EventArgs) Handles ButtonNine.Click
+        TextBox1.Text += "9"
+    End Sub
+
+    Private Sub ButtonZero_Click(sender As Object, e As EventArgs) Handles ButtonZero.Click
+        TextBox1.Text += "0"
+    End Sub
+
+    Private Sub ButtonPlus_Click(sender As Object, e As EventArgs) Handles ButtonPlus.Click
+        TextBox1.Text += "+"
+    End Sub
+
+    Private Sub ButtonMinus_Click(sender As Object, e As EventArgs) Handles ButtonMinus.Click
+        TextBox1.Text += "-"
+    End Sub
+
+    Private Sub ButtonDevide_Click(sender As Object, e As EventArgs) Handles ButtonDevide.Click
+        TextBox1.Text += "/"
+    End Sub
+
+    Private Sub ButtonMultiply_Click(sender As Object, e As EventArgs) Handles ButtonMultiply.Click
+        TextBox1.Text += "*"
+    End Sub
+
+    Private Sub ButtonExponent_Click(sender As Object, e As EventArgs) Handles ButtonExponent.Click
+        TextBox1.Text += "^"
+    End Sub
+
+    Private Sub ButtonEqual_Click(sender As Object, e As EventArgs) Handles ButtonEqual.Click
+        TextBox1.Text += "="
+        Console.WriteLine(TextBox1.Lines.ElementAt(0))
+        Dim string1 As String = TextBox1.Lines.ElementAt(0)
+        Dim Var1 As Integer = 0
+        Dim Var2 As Integer = 0
+        Dim OP As Char
+        Dim Answer As Integer = 0
+        Dim FirstVar As Boolean = True
+        For Each character As Char In string1
+            If IsNumeric(character) And FirstVar Then
+                Var1 += Val(character)
+                FirstVar = False
+            End If
+            If IsNumeric(character) And Not FirstVar Then
+                Var2 += Val(character)
+            End If
+            If Not IsNumeric(character) And character <> "=" Then
+                OP = character
+                Console.WriteLine("Triggered not equal if statement")
+            End If
+            If Not IsNumeric(character) And character = "=" Then
+                Select Case OP
+                    Case "+"
+                        Answer = Var1 + Var2
+                    Case "-"
+                        Answer = Var1 - Var2
+                    Case "/"
+                        Answer = Var1 / Var2
+                    Case "*"
+                        Answer = Var1 * Var2
+                    Case "^"
+                        Answer = Var1 ^ Var2
+                End Select
+                TextBox1.Text += Answer
+
+            End If
+
+        Next character
+
+    End Sub
+End Class
